5f253182de67112d41d915313cfc50021f12288a
Nathan Stocks <cleancut@github.com>
2019-12-07 21:33:03 -0700

    Completed section 2.1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io::{self, Read, Write};
+
+const CHUNK_SIZE: usize = 16 * 1024;
+
 fn main() {
-    println!("Hello, world!");
+    let mut total_bytes = 0;
+    loop {
+        let mut buffer = [0; CHUNK_SIZE];
+        let num_read = match io::stdin().read(&mut buffer) {
+            Ok(0) => break,
+            Ok(x) => x,
+            Err(_) => break,
+        };
+        total_bytes += num_read;
+        io::stdout().write_all(&buffer[..num_read]).unwrap();
+    }
+    eprintln!("{}", total_bytes);
+
 }
