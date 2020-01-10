d7374909d11a52636bf0fa0a01e7a5040458d8e2
Nathan Stocks <cleancut@github.com>
2020-01-10 13:36:49 -0700

    Completed section 4.2

--- src/stats.rs
+++ src/stats.rs
@@ -5,11 +5,20 @@ use std::time::Instant;
 pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
     let mut total_bytes = 0;
     let start = Instant::now();
+    let mut last_instant = Instant::now();
     loop {
         let num_bytes = stats_rx.recv().unwrap();
+        let now = Instant::now();
+        let rate_per_second = num_bytes as f64 / (now - last_instant).as_secs_f64();
+        last_instant = now;
         total_bytes += num_bytes;
         if !silent {
-            eprint!("\r{} {}", total_bytes, start.elapsed().as_secs());
+            eprint!(
+                "\r{} {} [{:.0}b/s]",
+                total_bytes,
+                start.elapsed().as_secs(),
+                rate_per_second
+            );
         }
         if num_bytes == 0 {
             break;
