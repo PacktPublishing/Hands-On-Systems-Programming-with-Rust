a5b0a19722e24f887faf4c4f1540d56ec1e5cb6a
Nathan Stocks <cleancut@github.com>
2020-01-10 11:48:30 -0700

    Completed section 4.1

--- src/stats.rs
+++ src/stats.rs
@@ -1,13 +1,15 @@
 use crossbeam::channel::Receiver;
 use std::io::Result;
+use std::time::Instant;
 
 pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
     let mut total_bytes = 0;
+    let start = Instant::now();
     loop {
         let num_bytes = stats_rx.recv().unwrap();
         total_bytes += num_bytes;
         if !silent {
-            eprint!("\r{}", total_bytes);
+            eprint!("\r{} {}", total_bytes, start.elapsed().as_secs());
         }
         if num_bytes == 0 {
             break;
