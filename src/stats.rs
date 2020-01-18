2f49d6ae20aed7f9a3721452ef1a84db8887dc9e
Nathan Stocks <cleancut@github.com>
2020-01-18 09:46:36 -0700

    Completed section 4.4

--- src/stats.rs
+++ src/stats.rs
@@ -5,18 +5,18 @@ use std::time::{Duration, Instant};
 pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
     let mut total_bytes = 0;
     let start = Instant::now();
-    let mut last_instant = Instant::now();
+    let mut timer = Timer::new();
     loop {
         let num_bytes = stats_rx.recv().unwrap();
-        let now = Instant::now();
-        let rate_per_second = num_bytes as f64 / (now - last_instant).as_secs_f64();
-        last_instant = now;
+        timer.update();
+        let rate_per_second = num_bytes as f64 / timer.delta.as_secs_f64();
         total_bytes += num_bytes;
-        if !silent {
+        if !silent && timer.ready {
+            timer.ready = false;
             eprint!(
                 "\r{} {} [{:.0}b/s]",
                 total_bytes,
-                start.elapsed().as_secs(),
+                start.elapsed().as_secs().as_time(),
                 rate_per_second
             );
         }
@@ -28,6 +28,18 @@ pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
         eprintln!();
     }
     Ok(())
+}
+
+trait TimeOutput {
+    fn as_time(&self) -> String;
+}
+
+impl TimeOutput for u64 {
+    fn as_time(&self) -> String {
+        let (hours, left) = (*self / 3600, *self % 3600);
+        let (minutes, seconds) = (left / 60, left % 60);
+        format!("{}:{:02}:{:02}", hours, minutes, seconds)
+    }
 }
 
 struct Timer {
